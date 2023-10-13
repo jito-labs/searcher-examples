51495653a687daef9867e4792955c9a53f004ea9
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2023-10-13 11:36:48 -0700

    Upgrade to 1.16 + Proto update (#32)

--- jito_protos/src/convert.rs
+++ jito_protos/src/convert.rs
@@ -1,6 +1,7 @@
 use std::{
     cmp::min,
-    net::{IpAddr, Ipv4Addr},
+    net::{AddrParseError, IpAddr, Ipv4Addr, SocketAddr},
+    str::FromStr,
 };
 
 use bincode::serialize;
@@ -10,9 +11,12 @@ use solana_sdk::{
     transaction::VersionedTransaction,
 };
 
-use crate::packet::{
-    Meta as ProtoMeta, Packet as ProtoPacket, PacketBatch as ProtoPacketBatch,
-    PacketFlags as ProtoPacketFlags,
+use crate::{
+    packet::{
+        Meta as ProtoMeta, Packet as ProtoPacket, PacketBatch as ProtoPacketBatch,
+        PacketFlags as ProtoPacketFlags,
+    },
+    shared::Socket,
 };
 
 /// Converts a Solana packet to a protobuf packet
@@ -21,17 +25,17 @@ pub fn packet_to_proto_packet(p: &Packet) -> Option<ProtoPacket> {
     Some(ProtoPacket {
         data: p.data(..)?.to_vec(),
         meta: Some(ProtoMeta {
-            size: p.meta.size as u64,
-            addr: p.meta.addr.to_string(),
-            port: p.meta.port as u32,
+            size: p.meta().size as u64,
+            addr: p.meta().addr.to_string(),
+            port: p.meta().port as u32,
             flags: Some(ProtoPacketFlags {
-                discard: p.meta.discard(),
-                forwarded: p.meta.forwarded(),
-                repair: p.meta.repair(),
-                simple_vote_tx: p.meta.is_simple_vote_tx(),
-                tracer_packet: false,
+                discard: p.meta().discard(),
+                forwarded: p.meta().forwarded(),
+                repair: p.meta().repair(),
+                simple_vote_tx: p.meta().is_simple_vote_tx(),
+                tracer_packet: p.meta().is_tracer_packet(),
             }),
-            sender_stake: p.meta.sender_stake,
+            sender_stake: 0,
         }),
     })
 }
@@ -39,90 +43,52 @@ pub fn packet_to_proto_packet(p: &Packet) -> Option<ProtoPacket> {
 pub fn packet_batches_to_proto_packets(
     batches: &[PacketBatch],
 ) -> impl Iterator<Item = ProtoPacket> + '_ {
-    batches.iter().flat_map(|b| {
-        b.iter().filter(|p| !p.meta.discard()).filter_map(|p| {
-            Some(ProtoPacket {
-                data: p.data(..)?.to_vec(),
-                meta: Some(ProtoMeta {
-                    size: p.meta.size as u64,
-                    addr: p.meta.addr.to_string(),
-                    port: p.meta.port as u32,
-                    flags: Some(ProtoPacketFlags {
-                        discard: p.meta.discard(),
-                        forwarded: p.meta.forwarded(),
-                        repair: p.meta.repair(),
-                        simple_vote_tx: p.meta.is_simple_vote_tx(),
-                        tracer_packet: false,
-                    }),
-                    sender_stake: p.meta.sender_stake,
-                }),
-            })
-        })
-    })
+    batches
+        .iter()
+        .flat_map(|b| b.iter().filter_map(packet_to_proto_packet))
 }
 
-// converts from a protobuf packet to packet
+/// converts from a protobuf packet to packet
 pub fn proto_packet_to_packet(p: &ProtoPacket) -> Packet {
-    let mut data = [0; PACKET_DATA_SIZE];
+    let mut data = [0u8; PACKET_DATA_SIZE];
     let copy_len = min(data.len(), p.data.len());
     data[..copy_len].copy_from_slice(&p.data[..copy_len]);
-    let mut packet = Packet::new(data, Default::default());
+    let mut packet = Packet::new(data, Meta::default());
     if let Some(meta) = &p.meta {
-        packet.meta.size = meta.size as usize;
-        packet.meta.addr = meta.addr.parse().unwrap_or(UNKNOWN_IP);
-        packet.meta.port = meta.port as u16;
+        packet.meta_mut().size = meta.size as usize;
+        packet.meta_mut().addr = meta
+            .addr
+            .parse()
+            .unwrap_or(IpAddr::V4(Ipv4Addr::UNSPECIFIED));
+        packet.meta_mut().port = meta.port as u16;
         if let Some(flags) = &meta.flags {
             if flags.simple_vote_tx {
-                packet.meta.flags.insert(PacketFlags::SIMPLE_VOTE_TX);
+                packet.meta_mut().flags.insert(PacketFlags::SIMPLE_VOTE_TX);
             }
             if flags.forwarded {
-                packet.meta.flags.insert(PacketFlags::FORWARDED);
+                packet.meta_mut().flags.insert(PacketFlags::FORWARDED);
             }
             if flags.tracer_packet {
-                packet.meta.flags.insert(PacketFlags::TRACER_PACKET);
+                packet.meta_mut().flags.insert(PacketFlags::TRACER_PACKET);
             }
             if flags.repair {
-                packet.meta.flags.insert(PacketFlags::REPAIR);
+                packet.meta_mut().flags.insert(PacketFlags::REPAIR);
+            }
+            if flags.discard {
+                packet.meta_mut().flags.insert(PacketFlags::DISCARD);
             }
         }
-        packet.meta.sender_stake = meta.sender_stake;
     }
     packet
 }
 
-const UNKNOWN_IP: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
 pub fn proto_packet_batch_to_packets(
     packet_batch: ProtoPacketBatch,
 ) -> impl Iterator<Item = Packet> {
-    packet_batch.packets.into_iter().map(|proto_packet| {
-        let mut packet = Packet::new([0; PACKET_DATA_SIZE], Meta::default());
-        let copy_len = min(PACKET_DATA_SIZE, proto_packet.data.len());
-        packet.buffer_mut()[..copy_len].copy_from_slice(&proto_packet.data[..copy_len]);
-        if let Some(meta) = &proto_packet.meta {
-            packet.meta.size = meta.size as usize;
-            packet.meta.addr = meta.addr.parse().unwrap_or(UNKNOWN_IP);
-            packet.meta.port = meta.port as u16;
-            if let Some(flags) = &meta.flags {
-                if flags.simple_vote_tx {
-                    packet.meta.flags.insert(PacketFlags::SIMPLE_VOTE_TX);
-                }
-                if flags.forwarded {
-                    packet.meta.flags.insert(PacketFlags::FORWARDED);
-                }
-                if flags.tracer_packet {
-                    packet.meta.flags.insert(PacketFlags::TRACER_PACKET);
-                }
-                if flags.repair {
-                    packet.meta.flags.insert(PacketFlags::REPAIR);
-                }
-                if flags.discard {
-                    packet.meta.flags.insert(PacketFlags::DISCARD);
-                }
-            }
-            packet.meta.sender_stake = meta.sender_stake;
-        }
-        packet
-    })
+    packet_batch
+        .packets
+        .into_iter()
+        .map(|proto_packet| proto_packet_to_packet(&proto_packet))
 }
 
 /// Converts a protobuf packet to a VersionedTransaction
@@ -132,9 +98,20 @@ pub fn versioned_tx_from_packet(p: &ProtoPacket) -> Option<VersionedTransaction>
     data[..copy_len].copy_from_slice(&p.data[..copy_len]);
     let mut packet = Packet::new(data, Default::default());
     if let Some(meta) = &p.meta {
-        packet.meta.size = meta.size as usize;
+        packet.meta_mut().size = meta.size as usize;
     }
     packet.deserialize_slice(..).ok()
+}
+
+/// Coverts a VersionedTransaction to packet
+pub fn packet_from_versioned_tx(tx: VersionedTransaction) -> Packet {
+    let tx_data = serialize(&tx).expect("serializes");
+    let mut data = [0; PACKET_DATA_SIZE];
+    let copy_len = min(tx_data.len(), data.len());
+    data[..copy_len].copy_from_slice(&tx_data[..copy_len]);
+    let mut packet = Packet::new(data, Default::default());
+    packet.meta_mut().size = copy_len;
+    packet
 }
 
 /// Converts a VersionedTransaction to a protobuf packet
@@ -150,6 +127,15 @@ pub fn proto_packet_from_versioned_tx(tx: &VersionedTransaction) -> ProtoPacket 
             flags: None,
             sender_stake: 0,
         }),
+    }
+}
+
+/// Converts a GRPC Socket to stdlib SocketAddr
+impl TryFrom<&Socket> for SocketAddr {
+    type Error = AddrParseError;
+
+    fn try_from(value: &Socket) -> Result<Self, Self::Error> {
+        IpAddr::from_str(&value.ip).map(|ip| SocketAddr::new(ip, value.port as u16))
     }
 }
 
--- searcher_client/src/lib.rs
+++ searcher_client/src/lib.rs
@@ -121,7 +121,7 @@ pub async fn send_bundle_with_confirmation(
                     Some(Reason::WinningBatchBidRejected(WinningBatchBidRejected {
                         auction_id,
                         simulated_bid_lamports,
-                        msg: _
+                        msg: _,
                     })) => {
                         return Err(Box::new(BundleRejectionError::WinningBatchBidRejected(
                             auction_id,
@@ -131,7 +131,7 @@ pub async fn send_bundle_with_confirmation(
                     Some(Reason::StateAuctionBidRejected(StateAuctionBidRejected {
                         auction_id,
                         simulated_bid_lamports,
-                        msg: _
+                        msg: _,
                     })) => {
                         return Err(Box::new(BundleRejectionError::StateAuctionBidRejected(
                             auction_id,
